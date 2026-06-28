295018f891da5c65b5eecb5bc5fdc5cb49ea559c
Roberto <andval19971997@gmail.com>
2026-06-28 17:57:50 +0200

    catalogo

---
+++ assets/productos-magnotex.js
@@ -0,0 +1,19 @@
+window.MAGNOTEX_PRODUCTS={
+  allProducts:[
+    {n:"Almohadas",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/almohadas-02.jpg"},
+    {n:"Colchas",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/colchas-02.jpg"},
+    {n:"Duvet",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/duvet-01.jpg"},
+    {n:"Fundas de duvet",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/fundas-duvet-11.jpg"},
+    {n:"Edredones",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/edredones-04.jpg"},
+    {n:"Protector de colchón",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/protector-de-duvet-01.jpg"},
+    {n:"Protector de almohada",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/protector-de-almohada-01.jpg"},
+    {n:"Toallas",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/toallas-01.jpg"},
+    {n:"Sábanas",img:"assets/magnotex_cama_1200x737.png"},
+    {n:"Salidas de baño",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/salidas-de-bano-05.jpg"},
+    {n:"Faldones",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/faldones-04.jpg"},
+    {n:"Cobija térmica",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/cobijas-termicas-06.jpg"},
+    {n:"Mantelería",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/manteleria-04.jpg"},
+    {n:"Alfombras de baño",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/alfombra-de-bano-01.jpg"}
+  ],
+  featuredProductNames:["Salidas de baño","Sábanas","Toallas","Almohadas"]
+};
