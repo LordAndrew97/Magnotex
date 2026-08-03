011183a2356d11733cc19c424d5c3f0d0e8af1e8
Roberto <andval19971997@gmail.com>
2026-08-03 20:30:59 +0200

    Revisar contenidos y localizar recursos

--- assets/productos-magnotex.js
+++ assets/productos-magnotex.js
@@ -1,19 +1,19 @@
 window.MAGNOTEX_PRODUCTS={
   allProducts:[
     {n:"Almohadas",img:"assets/producto-almohadas.png"},
-    {n:"Colchas",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/colchas-02.jpg"},
-    {n:"Duvet",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/duvet-01.jpg"},
-    {n:"Fundas de duvet",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/fundas-duvet-11.jpg"},
-    {n:"Edredones",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/edredones-04.jpg"},
-    {n:"Protector de colchón",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/protector-de-duvet-01.jpg"},
-    {n:"Protector de almohada",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/protector-de-almohada-01.jpg"},
+    {n:"Colchas",img:"assets/producto-colchas.jpg"},
+    {n:"Duvet",img:"assets/producto-duvet.jpg"},
+    {n:"Fundas de duvet",img:"assets/producto-fundas-duvet.jpg"},
+    {n:"Edredones",img:"assets/producto-edredones.jpg"},
+    {n:"Protector de colchón",img:"assets/producto-protector-colchon.jpg"},
+    {n:"Protector de almohada",img:"assets/producto-protector-almohada.jpg"},
     {n:"Toallas",img:"assets/producto-toallas.png"},
     {n:"Sábanas",img:"assets/producto-sabanas.png"},
     {n:"Salidas de baño",img:"assets/producto-salidas-bano.png"},
-    {n:"Faldones",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/faldones-04.jpg"},
-    {n:"Cobija térmica",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/cobijas-termicas-06.jpg"},
-    {n:"Mantelería",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/manteleria-04.jpg"},
-    {n:"Alfombras de baño",img:"https://edredonesmagnotex.com/wp-content/uploads/2023/08/alfombra-de-bano-01.jpg"}
+    {n:"Faldones",img:"assets/producto-faldones.jpg"},
+    {n:"Cobija térmica",img:"assets/producto-cobija-termica.jpg"},
+    {n:"Mantelería",img:"assets/producto-manteleria.jpg"},
+    {n:"Alfombras de baño",img:"assets/producto-alfombra-bano.jpg"}
   ],
   featuredProductNames:["Salidas de baño","Sábanas","Toallas","Almohadas"]
 };
